42fc8fc9a4ae8adbc0390e952083256c974cc35d
Adam Cross <adamscross04@gmail.com>
2025-03-13 01:45:06 +0000

    Added ProductDataMapper and interface

---
+++ Data.Mappers/Abstractions/IProductDataMapper.cs
@@ -0,0 +1,10 @@
+﻿using Common.Mappers.Abstractions;
+using Data.Entities;
+using Domain.Models;
+
+namespace Data.Mappers.Abstractions;
+
+/// <summary>
+/// Interface for mapping between ProductEntity and Product objects.
+/// </summary>
+public interface IProductDataMapper : ITwoWayMapper<ProductEntity, Product>;
---
+++ Data.Mappers/ProductDataMapper.cs
@@ -0,0 +1,44 @@
+﻿using Common.Mappers;
+using Data.Entities;
+using Data.Mappers.Abstractions;
+using Domain.Models;
+
+namespace Data.Mappers;
+
+/// <summary>
+/// Maps between ProductEntity and Product objects.
+/// </summary>
+public class ProductDataMapper: TwoWayMapperBase<ProductEntity, Product>, IProductDataMapper
+{
+    /// <summary>
+    /// Maps a Product object to a ProductEntity object.
+    /// </summary>
+    /// <param name="obj">The Product object to map.</param>
+    /// <returns>The mapped ProductEntity object.</returns>
+    public override ProductEntity Map(Product obj)
+    {
+        return new ProductEntity
+        {
+            Id = obj.Id,
+            Name = obj.Name,
+            Description = obj.Description,
+            Price = obj.Price
+        };
+    }
+
+    /// <summary>
+    /// Maps a ProductEntity object to a Product object.
+    /// </summary>
+    /// <param name="obj">The ProductEntity object to map.</param>
+    /// <returns>The mapped Product object.</returns>
+    public override Product Map(ProductEntity obj)
+    {
+        return new Product
+        {
+            Id = obj.Id,
+            Name = obj.Name,
+            Description = obj.Description,
+            Price = obj.Price
+        };
+    }
+}
