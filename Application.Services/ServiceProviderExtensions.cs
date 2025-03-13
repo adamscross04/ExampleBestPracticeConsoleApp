bebdf084f609aea32d7be434599f511a72151273
Adam Cross <adamscross04@gmail.com>
2025-03-13 16:37:38 +0000

    Added Application project, Application.Services project and added noddy worker service to prove it works

---
+++ Application.Services/ServiceProviderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services.Abstractions;
+using Application.Services.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Services;
+
+public static class ServiceProviderExtensions
+{
+    public static void RegisterApplicationServices(this IServiceCollection services)
+    {
+        services.AddTransient<IWorkerService, WorkerService>();
+    }
+}
