3467ca89f8e2a156b04b3ecdd2599125bb43d3d6
Adam Cross <adamscross04@gmail.com>
2025-03-13 02:48:04 +0000

    Adding AwesomeAssertions across the board and adding a wrapper around Dapper for testing purposes.

---
+++ Data.Dapper.Extensions/Abstractions/IDbConnectionWrapper.cs
@@ -0,0 +1,7 @@
+﻿namespace Data.Dapper.Extensions.Abstractions;
+
+public interface IDbConnectionWrapper
+{
+    Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null);
+    Task<T?> QuerySingleOrDefaultAsync<T>(string sql, object param = null);
+}
---
+++ Data.Dapper.Extensions/IDbConnectionWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using Dapper;
+using Data.Dapper.Extensions.Abstractions;
+
+namespace Data.Dapper.Extensions;
+
+public class DbConnectionWrapper(IDbConnection dbConnection) : IDbConnectionWrapper
+{
+    public Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null)
+    {
+        return dbConnection.QueryAsync<T>(sql, param);
+    }
+
+    public Task<T?> QuerySingleOrDefaultAsync<T>(string sql, object param = null)
+    {
+        return dbConnection.QuerySingleOrDefaultAsync<T>(sql, param);
+    }
+}
