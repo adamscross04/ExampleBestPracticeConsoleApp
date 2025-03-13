3e272f730501927423c278e3adc380ea85459c58
Adam Cross <adamscross04@gmail.com>
2025-03-13 01:14:52 +0000

    Creating IMappers

---
+++ Common.Mappers/IOneWayMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Mappers;
+
+/// <summary>
+///     Interface for mapping an object of type TIn to an object of type TOut.
+/// </summary>
+/// <typeparam name="TIn">The input type to be mapped from.</typeparam>
+/// <typeparam name="TOut">The output type to be mapped to.</typeparam>
+public interface IOneWayMapper<in TIn, out TOut>
+{
+    /// <summary>
+    /// Maps an object of type TIn to an object of type TOut
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    TOut Map(TIn obj);
+}
---
+++ Common.Mappers/ITwoWayMapper.cs
@@ -0,0 +1,23 @@
+﻿namespace Common.Mappers;
+
+/// <summary>
+///     Interface for mapping objects between two types T1 and T2.
+/// </summary>
+/// <typeparam name="T1">The first type to be mapped.</typeparam>
+/// <typeparam name="T2">The second type to be mapped.</typeparam>
+public interface ITwoWayMapper<T1, T2>
+{
+    /// <summary>
+    ///     Maps an object of type T2 to an object of type T1.
+    /// </summary>
+    /// <param name="obj">The object of type T2 to be mapped.</param>
+    /// <returns>The mapped object of type T1.</returns>
+    T1 Map(T2 obj);
+
+    /// <summary>
+    ///     Maps an object of type T1 to an object of type T2.
+    /// </summary>
+    /// <param name="obj">The object of type T1 to be mapped.</param>
+    /// <returns>The mapped object of type T2.</returns>
+    T2 Map(T1 obj);
+}
