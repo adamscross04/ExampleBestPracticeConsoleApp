787b3620fefdf95efe186434a16838b2d3645643
Adam Cross <adamscross04@gmail.com>
2025-03-13 16:27:19 +0000

    Exclude DbConnectionWrapper from tests as just abstraction for testing purposes

--- Data.Dapper.Extensions/IDbConnectionWrapper.cs
+++ Data.Dapper.Extensions/IDbConnectionWrapper.cs
@@ -1,9 +1,11 @@
 ﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using Dapper;
 using Data.Dapper.Extensions.Abstractions;
 
 namespace Data.Dapper.Extensions;
 
+[ExcludeFromCodeCoverage]
 public class DbConnectionWrapper(IDbConnection dbConnection) : IDbConnectionWrapper
 {
     public Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null)
