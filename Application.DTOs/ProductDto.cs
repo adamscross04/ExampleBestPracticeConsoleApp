0e14240b6c9392e9e5a95dcd53de0b90eff6f367
Adam Cross <adamscross04@gmail.com>
2025-03-13 01:14:33 +0000

    Creating Product DTO/Model/Entity

---
+++ Application.DTOs/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Application.DTOs;
+
+/// <summary>
+/// Data Transfer Object for Product.
+/// </summary>
+public class ProductDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ Domain.Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models;
+
+/// <summary>
+/// Domain model for a Product.
+/// </summary>
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+}
